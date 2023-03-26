8d2bc15503c9878ebf82f7197ab279981df4d137
kpen84 <kpen84@gmail.com>
2023-03-26 02:58:02 -0400

    fixed issue with landing mage causing overflow

--- script.js
+++ script.js
@@ -15,3 +15,5 @@ function myFunction() {
       moreText.style.display = "inline";
     }
   }
+
+  [].forEach.call($$("*"),function(a){a.style.outline="1px solid #"+(~~(Math.random()*(1<<24))).toString(16)})
