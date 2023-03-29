2e5bc4f65fbe2ec4be975c474c6113d76bd5cd08
kpen84 <kpen84@gmail.com>
2023-03-29 01:29:46 -0400

    working side nav

--- script.js
+++ script.js
@@ -32,5 +32,12 @@ function moreText() {
   }
 
 
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
 
 
