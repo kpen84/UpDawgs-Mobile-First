c22ffcb1d15471bd477c2d56577623ddde3921b2
kpen84 <kpen84@gmail.com>
2023-03-30 00:39:12 -0400

    completed

--- script.js
+++ script.js
@@ -14,23 +14,25 @@ function moreText() {
       btnText.innerHTML = "Read less";
       moreText.style.display = "inline";
     }
+}
+
+//Menu Tabs
+
+function openFood(evt, foodName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("food");
+  for (i = 0; i < x.length; i++) {
+  x[i].style.display = "none";
   }
-
-
-  function openFood(evt, foodName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("food");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(foodName).style.display = "flex";
-    evt.currentTarget.className += " active";
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
+  document.getElementById(foodName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
 
+//Sidebar Nav
 
 function openNav() {
   document.getElementById("mySidenav").style.left = "0";
@@ -40,4 +42,26 @@ function closeNav() {
   document.getElementById("mySidenav").style.left = "-1000px";
 }
 
+//Scroll Nav
+
+const sections = document.querySelectorAll("section[id]");
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  let scrollY = window.pageYOffset;
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = (current.getBoundingClientRect().top + window.pageYOffset) - 50;
+    sectionId = current.getAttribute("id");
+    
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
+}
 
